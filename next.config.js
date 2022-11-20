0f61f2ca35211550292910ae66da96d222b3e5f3
Abdul Ghofur <abdulghofur.me@gmail.com>
2022-11-20 09:17:44 +0700

    chore(next13): enable sass

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
     appDir: true
+  },
+  sassOptions: {
+    includesPaths: [path.join(__dirname, 'styles')]
   }
 }
 
